c187df98bd46ade9b66e63e0428c87c2c1b9c06d
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-22 13:42:26 +0900

    채팅을 위한 서버 세팅

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by khk on 2016-06-22.
+ */
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('chat.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
