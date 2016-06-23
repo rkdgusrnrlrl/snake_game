5aa1ab74dcd266aeb5b0d9255a64aeadc58aafc2
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-23 14:35:27 +0900

    서로 채팅 가능 하지 테스트

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
