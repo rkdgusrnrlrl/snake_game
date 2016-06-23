e616a6f132a04424dc91b10f48ad83745467f624
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-06-23 15:22:40 +0900

    클라이언트에서 값잘넘어가는지 테스트

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ io.on('connection', function(socket){
     console.log('a user connected');
 
     socket.on('chat message', function(msg){
+        console.log(msg);
         io.emit('msg', msg);
     });
 });
